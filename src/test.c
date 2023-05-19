41772a065671833e89cbabc04c2a088bcee66744
andreev-sergej <andreevsergej1111@gmail.com>
2023-05-19 14:20:16 +0300

    fix

--- src/test.c
+++ src/test.c
@@ -17,12 +17,18 @@ void run_test(char* testfile_path, int chain_start, int chain_end)
     FILE *file1 = fopen("output.txt", "r");
     FILE *file2 = fopen(testfile_path, "r");
     printf("\033[1m\033[33m%s\t\033[0m", testfile_path);
+
+
+    
     if(compare_files(file1, file2))
         printf("\033[1m\033[32mPASSED\033[0m\n");
     else
         printf("\033[1m\033[31mNOT PASSED\033[0m\n");
-        
+
     fclose(file1); 
+
+
+
 
     fclose(file2);
 }
